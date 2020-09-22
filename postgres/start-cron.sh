089eed4a26e60da2ea20909744bbe6f60152abb8
Sahil Malhotra <sahil@koverhoop.com>
2020-09-22 15:31:40 +0530

    creating env file inside container even if setup_cron is false

--- postgres/start-cron.sh
+++ postgres/start-cron.sh
@@ -1,5 +1,6 @@
+printenv | grep -v "affinity:container" | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
+chmod 777 /root/project_env.sh
+
 if [ $SETUP_CRON = true ]; then
-    printenv | grep -v "affinity:container" | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
-    chmod 777 /root/project_env.sh
     cron
 fi
